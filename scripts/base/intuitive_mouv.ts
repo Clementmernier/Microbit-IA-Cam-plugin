630f31ff65a358f41e59c0677795e6b2ab0ac124
Mernier <clement.mernier-garand@epita.fr>
2026-06-23 11:45:03 +0200

    trying one after another to put back blocks

--- scripts/base/intuitive_mouv.ts
+++ scripts/base/intuitive_mouv.ts
@@ -7,7 +7,7 @@
 /**
  * Mouvements simples pour le robot Maqueen Plus
  */
-//% weight=200 color=#FF6B35 icon="\uf1b9"
+//% weight=200 color=#FF6B35 icon="\uf1b9" block ="PSO Maqueen Mouvements"
 namespace PSO_Maqueen_movements {
     export enum Direction {
     //% block="gauche"
